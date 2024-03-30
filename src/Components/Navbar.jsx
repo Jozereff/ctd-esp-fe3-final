6cce9306977bddf28a0f8a1e067f4b9a48f90864
Jozereff <ojose9347@gmail.com>
2024-03-30 12:06:48 -0300

    Se agrega btn con cambio de tema

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { Link } from "react-router-dom"
 import { routes } from "./utils/routes"
 
-const Navbar = (theme, setTheme) => {
+const Navbar = ({theme, setTheme}) => {
 
 
   const changeTheme = () => {
